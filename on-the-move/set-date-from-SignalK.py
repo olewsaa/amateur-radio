b64e7d64396b0525df49c0c76ea489cd52c9088a
Ole W. Saastad <olewsaa@online.no>
2024-01-22 18:13:55 +0100

    Added som Q900 stuff

--- on-the-move/set-date-from-SignalK.py
+++ on-the-move/set-date-from-SignalK.py
@@ -22,7 +22,7 @@ import os, json, requests
 NTPrunning=os.system('ntpstat>/dev/null') 
 #print("NTP status", NTPrunning)
 if (NTPrunning != 0):    
-    #resp = requests.get('http://10.10.10.1:3000/signalk/v1/api/vessels/self/navigation/datetime/value', verify=False)a
+    #resp = requests.get('http://10.10.10.1:3000/signalk/v1/api/vessels/self/navigation/datetime/value', verify=False)
 
     resp = requests.get('http://demo.signalk.org/signalk/v1/api/vessels/self/navigation/datetime/value', verify=False)
 
